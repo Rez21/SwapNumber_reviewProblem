8e487a54c59349c694b810dc661ca471a4438332
Rez21 <banprasad21@gmail.com>
2022-08-12 11:07:30 +0530

    [Raghav] Add: swapping two number problem

---
+++ ReviewProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ReviewProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number Swap Problem");
+            Console.WriteLine("Enter a number select the method to swap 2 numbers");
+            Console.WriteLine("1. Using Addition Subtraction");
+            Console.WriteLine("2. Using Multiplication Division");
+            int ip = Convert.ToInt32(Console.ReadLine());
+            switch (ip)
+            {
+                case 1:
+                    SwapNumber a= new SwapNumber();
+                    a.Swap();
+
+                    break;
+                case 2: 
+                    SwapNumberMethodTwo b = new SwapNumberMethodTwo();
+                    
+                    break;
+
+
+            }
+        }
+    }
+
+}
---
+++ ReviewProblem/SwapNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewProblem
+{
+    class SwapNumber
+    {
+        public void Swap()
+        {
+            Console.WriteLine("Enter first Number: ");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second  Number: ");
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Intitially assigned numbers are : Num1= " + Num1+ " Num2= "+ Num2);
+
+
+            Num1 = Num1 + Num2;
+            Num2 = Num1 - Num2;
+            Num1 = Num1 - Num2;
+
+            Console.WriteLine("After Swapping:   Num1= "+ Num1+ " Num2= " +Num2);
+
+        }
+    }
+}
